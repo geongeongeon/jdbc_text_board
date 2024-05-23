80edcf9548169c5d8ebf7597c29dad3fb301bd36
geongeongeon <leegeonhee0204@gmail.com>
2024-05-23 17:27:10 +0900

    JDBC 연결

---
+++ text_board/db.sql
@@ -0,0 +1,4 @@
+#데이터 베이스 삭제 및 생성
+DROP DATABASE IF EXISTS text_board;
+CREATE DATABASE text_board;
+USE text_board;
---
+++ text_board/src/test/java/JDBCTest.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCTest {
+    public static void main(String[] args) {
+        // 데이터베이스 URL, 사용자 이름, 비밀번호를 설정합니다.
+        String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull"; // 데이터베이스 URL
+        //root 계정을 사용할 경우, user = "root", pw = ""
+        String user = "geonhee"; // 데이터베이스 사용자 이름
+        String password = "gh0204"; // 데이터베이스 비밀번호
+
+        // Connection 객체 선언
+        Connection conn = null;
+
+        try {
+            // 드라이버 로드 (필요에 따라 생략 가능, 최신 드라이버는 자동 로드)
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // 데이터베이스 연결
+            conn = DriverManager.getConnection(url, user, password);
+
+            if (conn != null) {
+                System.out.println("연결 성공!!");
+            } else {
+                System.out.println("연결 실패!!");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("JDBC Driver not found.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("연결 실패!!");
+        } finally {
+            // 연결 닫기
+            if (conn != null) {
+                try {
+                    conn.close();
+                    System.out.println("Connection closed.");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
